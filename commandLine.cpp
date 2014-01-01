041f13bddd9c89827b4229b6d058e27cef610d03
Vincent "MooZ" Cruz <cruz.vincent@gmail.com>
2014-01-01 21:51:32 +0100

    Code cleanup + minor comments

--- commandLine.cpp
+++ commandLine.cpp
@@ -6,60 +6,65 @@
 #include <iostream>
 // [debug::end]
 
-// [todo] COMMENTS!
-
 namespace Etripator   {
 namespace CommandLine {
 
+// Command line parser implementation.
 class Parser::Impl
 {
 	public:
+		// CTOR
 		Impl();
+		// DTOR
 		~Impl();
-		
+		// Parse configuration to build the list of supported arguments.
+		// It also pases the argv string and verify it complies with the
+		// passed configuration.
 		bool Parse(int argc, char** argv, std::string const& configuration);
+		// Check if the configuration contains a given argument.
 		bool Contains(std::string const& arg) const;
-		
+		// Parser configuration string.
 		bool ParseConfiguration(std::string const& configuration);
 	private:
-		struct Arguments
+		// Argument description.
+		struct Argument
 		{
 			std::string description;
 			std::string defaultValue;
 			std::vector<std::string> names;
 
-			typedef void (Arguments::*SetFunc)(std::string const& in, size_t start, size_t end);
+			typedef void (Argument::*SetFunc)(std::string const& in, size_t start, size_t end);
 
-			// [todo] CTOR and co?
+			Argument();
+			Argument(Argument const& arg);
+			~Argument();
+
+			Argument& operator= (Argument const& arg);
 
 			void SetDescription (std::string const& in, size_t start, size_t end);
 			void SetDefaultValue(std::string const& in, size_t start, size_t end);
 			void SetNames       (std::string const& in, size_t start, size_t end);
-		};
-};
+			
+			bool Parse(std::string const& in, size_t start, size_t end);
 
-Parser::Impl::Impl()
-{}
-		
-Parser::Impl::~Impl()
-{}
+			void Clear();
+		};
+
+		std::vector<Argument> _args;
+};
 
 bool Parser::Impl::ParseConfiguration(std::string const& configuration)
 {
+	bool ret;
 	size_t start, end;
-	size_t previous, separator, i;
 
-	Parser::Impl::Arguments arg;
+	Parser::Impl::Argument currentArg;
 
-	std::array<Arguments::SetFunc, 3> set = 
-	{ 
-		&Parser::Impl::Arguments::SetNames,
-		&Parser::Impl::Arguments::SetDefaultValue,
-		&Parser::Impl::Arguments::SetDescription
-	};
-
+	_args.clear();
 	for(start=0; ;)
 	{
+		currentArg.Clear();
+
 		start = configuration.find_first_of('[', start);
 		if(std::string::npos == start)
 		{
@@ -74,62 +79,126 @@ bool Parser::Impl::ParseConfiguration(std::string const& configuration)
 			return false;
 		}
 
-		separator = previous = start;
-		for(i=0; (previous < end) && (i < 3); i++)
-		{
-			separator = configuration.find_first_of('|', previous);
-			if((separator > end) || (std::string::npos == separator))
-			{
-				separator = end;
-			}
-			(arg.*set[i])(configuration, previous, separator);
+		ret = currentArg.Parse(configuration, start, end);
+		// [todo] ret
 
-			previous = separator+1;
-		}
-
-		if(3 != i)
-		{
-			// [todo] throw exception
-			return false;
-		}
+		_args.push_back(currentArg);
 	}
 
 	// At this point we must have at least one configuration.
-	// [todo] if() { throw ARG! }
+	if(_args.empty())
+	{
+		// [todo]
+		return false;
+	}
 	return true;
 }
 
-void Parser::Impl::Arguments::SetDescription (std::string const& in, size_t start, size_t end)
+bool Parser::Impl::Argument::Parse(std::string const& in, size_t start, size_t end)
+{
+	static const std::array<Argument::SetFunc, 3> set = 
+	{ 
+		&Parser::Impl::Argument::SetNames,
+		&Parser::Impl::Argument::SetDefaultValue,
+		&Parser::Impl::Argument::SetDescription
+	};
+
+	size_t i, current, previous;
+	for(i=0, previous=start; (previous < end) && (i < 3); i++, previous=current+1)
+	{
+		current = in.find_first_of('|', previous);
+		if((current > end) || (std::string::npos == current))
+		{
+			current = end;
+		}
+		(this->*set[i])(in, previous, current);
+	}
+
+	return (set.size() != i);
+}
+
+Parser::Impl::Argument::Argument()
+{}
+Parser::Impl::Argument::Argument(Parser::Impl::Argument const& arg)
+	: description(arg.description)
+	, defaultValue(arg.defaultValue)
+	, names(arg.names)
+{}
+Parser::Impl::Argument::~Argument()
+{}
+Parser::Impl::Argument& Parser::Impl::Argument::operator= (Parser::Impl::Argument const& arg)
+{
+	description  = arg.description;
+	defaultValue = arg.defaultValue;
+	names        = arg.names;
+	return *this;
+}
+
+void Parser::Impl::Argument::SetDescription (std::string const& in, size_t start, size_t end)
 {
 	size_t len = ((end < in.size()) ? end : in.size()) - start;
 	description = in.substr(start, len);
 }
 
-void Parser::Impl::Arguments::SetDefaultValue(std::string const& in, size_t start, size_t end)
+void Parser::Impl::Argument::SetDefaultValue(std::string const& in, size_t start, size_t end)
 {
 	size_t len = ((end < in.size()) ? end : in.size()) - start;
 	defaultValue = in.substr(start, len);	
 }
 
-void Parser::Impl::Arguments::SetNames(std::string const& in, size_t start, size_t end)
+void Parser::Impl::Argument::SetNames(std::string const& in, size_t start, size_t end)
 {
-	size_t len = ((end < in.size()) ? end : in.size()) - start;
-	// [todo] split by space/tab
-	// [todo] then check for extra infos
-	names.push_back(in.substr(start, len));
+	size_t current, next;
+
+	names.clear();
+	for(current=start; (std::string::npos!=current) && (current < end);)
+	{
+		current = in.find_first_not_of(" \t", current, end-current);
+		if((std::string::npos!=current) && (current < end))
+		{
+			next = in.find_first_of(" \t", current, end-current);
+			if((std::string::npos == next) || (next > end))
+			{
+				next = end;
+			}
+			names.push_back(in.substr(current, next-current));
+			current=next;
+		}
+	}
 }
 
+void Parser::Impl::Argument::Clear()
+{
+	description.clear();
+	defaultValue.clear();
+	for(size_t i=0; i<names.size(); i++)
+	{
+		names[i].clear();
+	}
+	names.clear();
+}
+
+// CTOR
+Parser::Impl::Impl()
+{}
+// DTOR	
+Parser::Impl::~Impl()
+{}
+// Parse configuration to build the list of supported arguments.
+// It also pases the argv string and verify it complies with the
+// passed configuration.
 bool Parser::Impl::Parse(int argc, char** argv, std::string const& configuration)
 {
 	bool ret;
 
 	ret = ParseConfiguration(configuration);
-
-	// [todo]
-
+	if(ret)
+	{
+		// [todo] parse argv
+	}
 	return ret;
 }
-
+// Check if the configuration contains a given argument.
 bool Parser::Impl::Contains(std::string const& arg) const
 {
 	// [todo]
@@ -152,9 +221,15 @@ bool Parser::Parse(int argc, char** argv, std::string const& configuration)
 	}
 
 	_impl = new Parser::Impl;
-	_impl->Parse(argc, argv, configuration);
+	return _impl->Parse(argc, argv, configuration);
+}
 
-	return true;
+//! Display general description and argument description.
+//! \param [in] general    General description.
+void Parser::Usage(std::string const& general)
+{
+	std::cout << general << std::endl;
+	// [todo]
 }
 
 bool Parser::Contains(std::string const& arg) const
--- commandLine.h
+++ commandLine.h
@@ -6,16 +6,39 @@
 
 namespace Etripator   {
 namespace CommandLine {
-
+	//! Command line argument parser.
 	class Parser
 	{
 		public:
+			//! Default constructor.
 			Parser();
+			//! Destructor.
 			~Parser();
 
+			//! Parse configuration to build the list of supported arguments.
+			//! It also pases the argv string and verify it complies with the
+			//! passed configuration.
+			//! \param [in] argc             Argument count.
+			//! \param [in] argv             Arguments string.
+			//! \param [in] configuration    Argument configuration.
+			// (todo)
+			//! \code{.cpp}
+			//! std::string cfg = "[h help usage|    |Display this message]"
+			//!                   "[count c     |1   |Count option        ]"
+			//!                   "[t threshold |0.5 |Threshold value     ]";
+			//! \endcode
+			//! \return (todo)
 			bool Parse(int argc, char** argv, std::string const& configuration);
+			//! Display general description and argument description.
+			//! \param [in] general    General description.
+			void Usage(std::string const& general);
+			//! (todo)
+			//! \param [in] arg    (todo)
+			//! \return (todo)
 			bool Contains(std::string const& arg) const;
-			
+			//! (todo)
+			//! \param [in] arg    (todo)
+			//! \return (todo)
 			template <typename T>
 			T Get(std::string const& arg) const; // [todo] ...
 
